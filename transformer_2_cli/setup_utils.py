26133b2f985021b999bc989388cd6ba32f7770e6
mingruimingrui <mingrui.wang@shopee.com>
2019-10-02 00:02:29 +0800

    added cli tools

---
+++ transformer_2_cli/setup_utils.py
@@ -0,0 +1,153 @@
+import os
+import logging
+from six import string_types
+from transformers_2.utils.io_utils import write_yaml_to_file
+
+logger = logging.getLogger()
+
+__all__ = [
+    'get_log_dir',
+    'get_data_dir',
+    'get_data_filepath',
+    'get_checkpoint_dir',
+    'get_output_dir',
+    'get_spm_model_prefix',
+    'get_cached_config_path',
+    'setup_logging',
+    'do_setup'
+]
+
+DEFAULT_LOGGING_FORMAT = (
+    '%(asctime)s '
+    '%(levelname)-4.4s '
+    '%(filename)s:%(lineno)d: '
+    '%(message)s'
+)
+
+
+def makedirs(path):
+    if not os.path.isdir(path):
+        os.makedirs(path)
+
+
+# --------------------------------------------------------------------------- #
+# Paths
+# --------------------------------------------------------------------------- #
+
+def get_log_dir(config):
+    return os.path.join(config.train_dir, 'logs')
+
+
+def get_data_dir(config):
+    return os.path.join(config.train_dir, 'data')
+
+
+def get_data_filepath(config, prefix, postfix=None):
+    filepath = os.path.join(get_data_dir(config), prefix)
+    if postfix is not None:
+        filepath = '{}.{}'.format(filepath, postfix)
+    return filepath
+
+
+def get_checkpoint_dir(config):
+    return os.path.join(config.train_dir, 'checkpoints')
+
+
+def get_output_dir(config):
+    return os.path.join(config.train_dir, 'output')
+
+
+def get_spm_model_prefix(config, src_or_tgt='src'):
+    output_dir = get_output_dir(config)
+    return os.path.join(output_dir, 'spm_model.{}').format(src_or_tgt)
+
+
+def get_spm_model_path(config, src_or_tgt='src'):
+    prefix = get_spm_model_prefix(config, src_or_tgt)
+    return '{}.model'.format(prefix)
+
+
+def get_cached_config_path(config):
+    return os.path.join(get_output_dir(config), 'config.yaml')
+
+
+# --------------------------------------------------------------------------- #
+# Logging and setup
+# --------------------------------------------------------------------------- #
+
+def setup_logging(
+    logger_or_name=None,
+    logfile=None,
+    log_to_console=True,
+    level=logging.INFO
+):
+    """
+    Sets up a logger for logging
+    Args:
+        logger_or_name: Either a logging.Logger object or a string
+            used to reference the logger name. If None, root logger is assumed.
+        logfile: If logging should be output to file, then provide a path
+            to store logging outputs to
+        log_to_console: Should logger display output to console?
+        level: The logging level
+    """
+
+    # Set root logger level
+    logging.root.setLevel(level)
+
+    # Get logger
+    if isinstance(logger_or_name, string_types):
+        logger = logging.getLogger(logger_or_name)
+    else:
+        logger = logger_or_name
+    logger.setLevel(level)
+
+    if log_to_console:
+        # Setup console logging
+        console_handler = logging.StreamHandler()
+        logger.addHandler(console_handler)
+
+    if logfile is not None:
+        file_handler = logging.FileHandler(
+            logfile,
+            mode='w',
+            encoding='utf-8',
+            delay=False
+        )
+        formatter = logging.Formatter(DEFAULT_LOGGING_FORMAT)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    return logger
+
+
+def do_setup(config, logger_or_name=None, logfile_prefix='train'):
+    """ Setup train directory folders and logger """
+    assert config.train_dir is not None, \
+        'train_dir is a required field, please give it a value'
+
+    # Make all output directories
+    makedirs(config.train_dir)
+    makedirs(get_log_dir(config))
+    makedirs(get_checkpoint_dir(config))
+    makedirs(get_data_dir(config))
+    makedirs(get_output_dir(config))
+
+    log_dir = get_log_dir(config)
+
+    # # Setup progress dict
+    # progress_dict_filepath = get_progress_dict_path(config)
+    # if not os.path.isfile(progress_dict_filepath):
+    #     write_json_to_file({}, progress_dict_filepath)
+
+    # Setup empty cached config
+    cached_config_filepath = get_cached_config_path(config)
+    if not os.path.isfile(cached_config_filepath):
+        write_yaml_to_file({}, cached_config_filepath)
+
+    # Setup logger
+    setup_logging(
+        logger_or_name=logger,
+        logfile=os.path.join(log_dir, '{}.log'.format(logfile_prefix)),
+        log_to_console=True
+    )
---
+++ transformer_2_cli/train_config.py
@@ -0,0 +1,207 @@
+"""
+Configurables for training
+"""
+
+from copy import deepcopy
+from transformers_2.utils.config_system import ConfigSystem
+
+__all__ = ['make_config']
+
+
+_C = ConfigSystem()
+# --------------------------------------------------------------------------- #
+# Start of general configs
+# --------------------------------------------------------------------------- #
+
+# `train_dir` (type: int) (required)
+# The directory to store training outputs and artifacts
+_C.train_dir = None
+
+# `num_workers` (type: int) (default: 2)
+# How many workers should be used to perform processing of corpus
+_C.num_workers = 2
+
+# `src_corpus_paths` (type: list[str]) (required)
+# List of paths to the source language corpus
+_C.src_corpus_paths = []
+
+# `tgt_corpus_paths` (type: list[str]) (required)
+# List of paths to the target language corpus
+_C.tgt_corpus_paths = []
+
+# `src_valid_path` (type: list[str]) (default: None)
+# Path to a source langauge validation file
+_C.src_valid_path = None
+
+# `tgt_valid_path` (type: list[str]) (default: None)
+# Path to a target langauge validation file
+_C.tgt_valid_path = None
+
+# --------------------------------------------------------------------------- #
+# Start of preprocessing configs
+# --------------------------------------------------------------------------- #
+
+# `src_preprocessing_steps` (type: list[processor_config]) (default: None)
+# The steps applied to the source language corpus to do data cleaning
+_C.src_preprocessing_steps = []
+
+# `tgt_preprocessing_steps` (type: list[processor_config]) (default: None)
+# The steps applied to the target language corpus to do data cleaning
+_C.tgt_preprocessing_steps = []
+
+
+# --------------------------------------------------------------------------- #
+# Start of tokenization configs
+# --------------------------------------------------------------------------- #
+
+# `src_spm_configs` are a set of configs dealing
+# with the way the source language is tokenized
+_C.src_spm_configs = ConfigSystem()
+
+# `use_existing` (type: string) (default: None)
+# If an existing spm model should be used, please provide a path
+# to the spm model file
+_C.src_spm_configs.use_existing = None
+
+# `vocab_size` (type: integer) (default: 8000)
+# The number of unique tokens
+# Recommended to set to a value of 8000, 16000 or 32000
+_C.src_spm_configs.vocab_size = 8000
+
+# `character_coverage` (type: float) (default: 0.9995)
+# The minimum percentage of characters to cover without the need for
+# <unk> token (aka unkown token)
+_C.src_spm_configs.character_coverage = 0.9995
+
+# `model_type` (type: string) (default: 'bpe')
+# The type of sentencepiece training to perform
+# Should be one of [bpe, unigram, word, char] default bpe
+_C.src_spm_configs.model_type = 'bpe'
+
+# `input_sentence_size` (type: integer) (default: 10000000)
+# To ensure that sentencepiece can train in a resonable amount of time,
+# a subset of sentences can be selected
+# By default a maximum of 1e7 (10 million) sentences will be used, to use all
+# sentences, set this value to 0
+_C.src_spm_configs.input_sentence_size = 10000000
+
+# `add_digit_tokens` (type: boolean) (default: False)
+# Adds 0-9 and ▁0-▁9 to user_defined_symbols
+_C.src_spm_configs.add_digit_tokens = False
+
+# The sentencepiece configs for the target language
+# options are the same as for source language
+_C.tgt_spm_configs = deepcopy(_C.src_spm_configs)
+
+
+# --------------------------------------------------------------------------- #
+# Model configs
+# --------------------------------------------------------------------------- #
+
+# This variable cannot be changed at this point of time
+_C.model_type = 'Transformer'
+
+# `model_config` is the model config for the transformer model
+_C.model_config = ConfigSystem()
+
+
+# --------------------------------------------------------------------------- #
+# Training configs
+# --------------------------------------------------------------------------- #
+
+# `train_configs` are a set of configs dealing
+# with training parameters like steps and batch size
+_C.train_configs = ConfigSystem()
+
+# `num_steps` (type: integer) (default: 100000)
+# The number of steps to train model for.
+_C.train_configs.num_steps = 100000
+
+# `update_freq` (type: integer) (default: 1)
+# Gradients will be accumulated and updated every this number of steps/
+_C.train_configs.update_freq = 1
+
+# `max_batch_tokens` (type: integer) (default: 1024)
+# The maxinum number of tokens to use per batch.
+_C.train_configs.max_batch_tokens = 1024
+
+# `max_batch_sentences` (type: integer) (default: 1024)
+# The maxinum number of sentences to use per batch.
+_C.train_configs.max_batch_sentences = 1024
+
+# # `train_configs` (type: string) (default: 'cross_entropy')
+# # The type of loss function to use.
+# # Option of [cross_entropy]
+# _C.train_configs.criterion = 'cross_entropy'
+
+# `lr` (type: float) (default: 0.00001)
+# The learning rate.
+_C.train_configs.lr = 0.00001
+
+# `warmup_steps` (type: int) (default: 16000)
+# The number of steps to do optimizer warmup.
+_C.train_configs.warmup_steps = 16000
+
+# `warmup_init_lr` (type: float) (default: 1e-7)
+# The initial learning rate for the warmup period.
+_C.train_configs.warmup_init_lr = 1e-7
+
+# `lr_scheduler` (type: string) (default: 'fixed')
+# (options: ['fixed', 'inverse_time', 'inverse_sqrt'])
+# The way learning rate changes during training
+# after warm up.
+_C.train_configs.lr_scheduler = 'fixed'
+
+# `min_lr` (type: float) (default: 1e-9)
+# The minimum learning rate as a result lr scheduler.
+_C.train_configs.min_lr = 1e-9
+
+# `adam_betas` (type: string) (default: '(0.9,0.98)')
+# The adam beta1 and beta2. The input to this argument must be a string
+# representation of a pair of float.
+_C.train_configs.adam_betas = '(0.9,0.98)'
+
+# `clipnorm` (type: float) (default: None)
+# Should gradient be clipped by norm of each tensor?
+# This should be the clipnorm value.
+_C.train_configs.clipnorm = None
+
+# `fp16` (type: boolean) (default: False)
+# Should fp16 training be used?
+_C.train_configs.fp16 = False
+
+# `dropout` (type: float) (default: 0.0)
+# Dropout probability
+_C.train_configs.dropout = 0.0
+
+# `dropout` (type: float) (default: 0.0)
+# Dropout probability for attention weights
+_C.train_configs.attn_dropout = 0.0
+
+# `dropout` (type: float) (default: 0.0)
+# Dropout probability after attention in transistor
+_C.train_configs.activation_dropout = 0.0
+
+
+# --------------------------------------------------------------------------- #
+# Logging configs
+# --------------------------------------------------------------------------- #
+
+# `log_configs` are a set of configs dealing
+# with logging matters
+_C.log_configs = ConfigSystem()
+
+# `log_interval` (type: int) (default: 60)
+# The interval in seconds to do logging
+_C.log_configs.log_interval = 60
+
+# `checkpoint_interval` (type: int) (default: 7200)
+# The interval in seconds to do logging
+_C.log_configs.checkpoint_interval = 7200
+
+
+# --------------------------------------------------------------------------- #
+# End of configs
+# --------------------------------------------------------------------------- #
+_C.immutable(True)
+make_config = _C.make_config
